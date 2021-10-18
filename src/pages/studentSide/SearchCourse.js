f8294eef829f8720d753788542ab0554b511cc6e
HaidarAliN <haidaralinehme@gmail.com>
2021-10-18 18:03:10 +0300

    completing the search page

--- src/pages/studentSide/SearchCourse.js
+++ src/pages/studentSide/SearchCourse.js
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader, Grid, IconButton, makeStyles, Typography } from '@material-ui/core'
+import { Card, CardContent, CardHeader, Grid, Button, InputLabel, makeStyles, TextField, Typography } from '@material-ui/core'
 import React, {useState, useEffect} from 'react'
 import Layout from '../../components/layouts/studentSideLayout/Layout'
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
@@ -6,6 +6,8 @@ import SearchIcon from '@material-ui/icons/Search';
 import BASE_API_URL from '../../services/BaseUrl'
 import axios from "axios";
 import { useHistory, useLocation } from 'react-router-dom'
+import ArrowForwardSharpIcon from '@material-ui/icons/ArrowForwardSharp';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
 
 const useStyles = makeStyles((theme) => ({
     card:{
@@ -33,21 +35,59 @@ const useStyles = makeStyles((theme) => ({
 export default function SearchCourse() {
     const [access_token, setAccess_token] = useState(JSON.parse( localStorage.getItem('access_token') ));
     const [data, setData] = useState(null);
+    const [status, setStatus] = useState(false);
+    const [searchName, setSearchName] = useState('');
+    const [searchNameError, setSearchNameError] = useState(false);
+    const [enrollMessage, setEnrollMessage] = useState(false);
     const classes = useStyles();
     const history = useHistory();
 
+        const handleSearch = async() => {
+            setSearchNameError(false);
+            if(!searchName){
+                setSearchNameError(true);
+            }
 
-    useEffect(async () => {
-        const response = await axios.get(`${BASE_API_URL}/api/student/get-user-info`,
-          {headers:{
-            'Authorization' : `Bearer ${access_token}`
-          }}
-        );
-        const data_fetched = response.data;
-        setData(data_fetched);
-        }, []);
-    
-  
+            if(searchName){
+                const response = await axios.post(`${BASE_API_URL}/api/student/search-for-course`,
+                {
+                    "course_name":searchName,
+                },
+                {headers:{
+                    'Authorization' : `Bearer ${access_token}`
+                }}
+                );
+                const data_fetched = response.data;
+                if(data_fetched.status){
+                    setData('');
+                    setStatus(true);
+                     setEnrollMessage(false);
+                }
+                else{
+                    setData(data_fetched);
+                    setEnrollMessage(false);
+                    setStatus(false);
+                }
+            }
+        }
+
+        const handleSubmit = async (id) => {
+            const response = await axios.post(`${BASE_API_URL}/api/student/enroll-in-course`,
+            {
+                "course_id":id,
+            },
+            {headers:{
+                'Authorization' : `Bearer ${access_token}`
+            }}
+            );
+            const data_fetched = response.data;
+            if(data_fetched){
+                console.log(id);
+                setSearchName('');
+                setEnrollMessage(true);
+                setData('');
+            }
+        }
 
     return (
     <Layout title="qwe" >
@@ -61,16 +101,41 @@ export default function SearchCourse() {
                 <div>
                     <Card elevation={1} className={classes.cardbody}>
                         <CardHeader
-                            title="Courses"
+                            title="Search box"
                             action={
-                                <IconButton>
-                                  <SearchIcon/>
-                                </IconButton>
+                                <div className={classes.btn}>
+                                    <Button
+                                        color="secondary" 
+                                        variant="contained"
+                                        onClick={handleSearch}
+                                        endIcon={<SearchIcon/>}>
+                                        Add Chapter
+                                    </Button>
+                                </div>
                               }
                         />
                         <CardContent>
-                        <Typography variant="body2" color="textSecondary">
-                        </Typography>
+                            <Grid container spacing={3}>
+                                <Grid item xs={12} md={3} lg={3} key={2}>
+                                    <InputLabel  className={classes.label}>Course Name:</InputLabel>
+                                </Grid>
+                                <Grid item xs={12} md={5} lg={5} key={3}>
+                                    <TextField
+                                        key={555}
+                                        onChange={(e) => setSearchName(e.target.value)}
+                                        className={classes.field}
+                                        variant="outlined"
+                                        color="primary"
+                                        fullWidth
+                                        value={searchName}
+                                        required
+                                        error={searchNameError}
+                                        placeholder="Name"
+                                    />
+                                </Grid>
+                                <Grid item xs={12} md={4} lg={4} key={4}>
+                                </Grid>
+                            </Grid>
                         </CardContent>
                     </Card>
                 </div>
@@ -78,10 +143,72 @@ export default function SearchCourse() {
                
             </Grid>
             </div>
+
+            {/* search card */}
+            <div className={classes.card}>
+            <Grid container spacing={1} >
+
+            {data &&
+                data.map(item => (
+
+                <Grid item xs={12}  sm={6} md={6} lg={4} key={item.id}>
+                <div>
+                    <Card elevation={1} className={classes.cardbody}
+                    >
+                        <CardHeader
+                            title={item.name}
+                            action={
+                                <MenuBookIcon/>
+                            }
+                            className={classes.cardHeader}
+                        />
+                        <CardContent className={classes.cardBody}>
+
+                           
+                            <div>
+                            <Typography>Progress: {item.progress}%</Typography>
+                            <Typography >Course Type: {item.course_type}</Typography>
+                            <div className={classes.btn}>
+                                <Button
+                                    
+                                    color="secondary" 
+                                    variant="contained"
+                                    onClick={() => handleSubmit(item.id)}
+                                    startIcon={<ArrowForwardSharpIcon />}
+                                    >
+                                    Enroll in Course
+                                </Button>
+                                </div>
+                             </div>                          
+
+                      
+                        
+                        </CardContent>
+                    </Card>
+                </div>
+                </Grid>
+                  
+
+                ))
+                            
+                        }
+                        {status && 
+                <Typography  component="h2"  variant="h4" >
+                    Nothing to show
+                </Typography>}
+            {enrollMessage &&
+                    <Typography  component="h2"  variant="h4" >
+                    Request Sent!
+                    </Typography>
+            }
+                          
+                        
+
+            </Grid>
+            </div>
+            {/* end search card */}
         </div>
-        <div className={classes.footer}>
-                            <Typography className={classes.footer}></Typography>
-        </div>
+      
     </Layout>
 
     )
