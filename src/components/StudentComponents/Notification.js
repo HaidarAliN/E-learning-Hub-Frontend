65a7ef9dcb717682c5665aa38d3e18cd0d23598e
HaidarAliN <haidaralinehme@gmail.com>
2021-11-06 20:10:06 +0200

    update student components

--- src/components/StudentComponents/Notification.js
+++ src/components/StudentComponents/Notification.js
@@ -75,6 +75,7 @@ export default function Notification({ data, handleRead, NavigateToCourse }) {
   const handleNavigate = (course_id, id) => {
     NavigateToCourse(course_id);
     markAsRead(id);
+    localStorage.setItem("location", "Notifications");
   };
   return (
     <TableContainer component={Paper}>
